88b0d26a7aca1757bb51192a28f32d1ebf73ad17
Alena Pumprová <alenapumprova@seznam.cz>
2026-05-31 23:23:27 +0200

    Add IE About dialog with custom content

--- src/components/AboutDialog.tsx
+++ src/components/AboutDialog.tsx
@@ -9,6 +9,8 @@ import FileManagerIcon from '../img/FolderClosed.webp';
 import WMPIcon from '../img/WindowsMediaPlayer 9.webp';
 import PaintIcon from '../img/Paint.webp';
 import NotepadIcon from '../img/Notepad.webp';
+import IELogo from '../img/IE6_about_logo.PNG.webp';
+import IEFile from '../img/URL.webp';
 
 import './AboutDialog.css';
 import '../App.css';
@@ -32,8 +34,68 @@ interface AboutDialogProps {
 const AboutDialog = ({ onClose, style, title }: AboutDialogProps) => {
     const { dialogRef, onMouseDown, draggableStyle } = useDraggableDialog();
     const icon = APP_ICONS[title];
-
     const size = TERMINAL_APPS.find(a => a.name === title)?.size;
+
+    if (title === 'Internet Explorer') {
+        return (
+            <div
+                id='about'
+                className='app-window about-dialog about-dialog--ie'
+                style={{ ...style, ...draggableStyle }}
+                ref={dialogRef}
+                tabIndex={-1}
+                onMouseDown={onMouseDown}
+            >
+                <div className='title-bar'>
+                    <span className='title-bar-text'>
+                        <img className='file-icon' src={IEFile} alt='' aria-hidden='true' />
+                        About Internet Explorer
+                    </span>
+                    <div className='title-bar-buttons xp-title-controls'>
+                        <button
+                            type='button'
+                            className='xp-title-control btn-close'
+                            onClick={onClose}
+                            aria-label='Close'
+                        >
+                            ✕
+                        </button>
+                    </div>
+                </div>
+
+                <div className='about-body about-body--ie'>
+                    <div className='about-ie-logo'>
+                        <img src={IELogo} alt='Internet Explorer' />
+                    </div>
+
+                    <div className='about-ie-info'>
+                        <p>Version: 6.0.2600.0000.xpclient.010817-1148</p>
+                        <p>Cipher Strength: 128-bit</p>
+                        <p>Product ID: 55274-640-1839662-23603</p>
+                        <p>Update Versions: 0</p>
+                        {size && <p>Application size: {size} KB</p>}
+                    </div>
+
+                    <div className='about-ie-scroll'>
+                        <p>This is a browser simulation built as part of a Windows XP Online Edition portfolio project. Visit <a href='https://alena-pumprova.cz/' target='_blank' rel='noopener noreferrer'>alena-pumprova.cz</a> to see more projects.</p>
+                    </div>
+
+                    <div className='about-ie-footer'>
+                        <img src={logo} alt='' className='about-ie-windows-logo' aria-hidden='true' />
+                        <div className='about-ie-links'>
+                            <a href='https://alena-pumprova.cz/' target='_blank' rel='noopener noreferrer'>
+                                Alena Pumprová — Portfolio
+                            </a>
+                            <a href='https://alena-pumprova.cz/' target='_blank' rel='noopener noreferrer'>
+                                About the author
+                            </a>
+                        </div>
+                        <button type='button' className='luna-btn' onClick={onClose} autoFocus>OK</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div
@@ -47,18 +109,17 @@ const AboutDialog = ({ onClose, style, title }: AboutDialogProps) => {
             <div className='title-bar'>
                 <div className='title-bar-text'>About {title}</div>
                 <div className='title-bar-buttons xp-title-controls'>
-                      <button
-                            type='button'
-                            className='xp-title-control btn-close'
-                            onClick={onClose}
-                            aria-label='Close'
-                        >
-                            ✕
-                        </button>
+                    <button
+                        type='button'
+                        className='xp-title-control btn-close'
+                        onClick={onClose}
+                        aria-label='Close'
+                    >
+                        ✕
+                    </button>
                 </div>
             </div>
 
-            {/* Top Banner */}
             <div className='about-banner'>
                 <div className='about-copy'>
                     <span>Copyright &copy; 1985-2001</span>
@@ -74,17 +135,14 @@ const AboutDialog = ({ onClose, style, title }: AboutDialogProps) => {
                 <span className='about-company'>Microsoft</span>
             </div>
 
-            {/* Body */}
             <div className='about-body'>
                 {icon && <img src={icon} className='about-app-icon' alt='' aria-hidden='true' />}
-
                 <div className='about-content'>
                     <div className='about-app-text'>
                         <p>Microsoft &#174; {title}</p>
                         <p>Version 5.1 (Build 2600.xpclient.010817-1148)</p>
                         <p>Copyright &copy; 1981-2001 Microsoft Corporation</p>
                     </div>
-
                     <p className='about-license'>
                         This product is licensed under the terms of the{' '}
                         <a href='https://alena-pumprova.cz/' target='_blank' rel='noopener noreferrer'>
@@ -92,11 +150,8 @@ const AboutDialog = ({ onClose, style, title }: AboutDialogProps) => {
                         </a>{' '}
                         to:
                     </p>
-
                     <p className='about-username'>Alena</p>
-
                     <hr />
-
                     {size && (
                         <p className='about-memory'>
                             Physical memory available to Windows: &nbsp; {size} KB
