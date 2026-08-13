af125b28844b7d544039493b00aea69f1031b12b
Alena Pumprová <alenapumprova@seznam.cz>
2026-08-14 00:25:22 +0200

    ire update

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
                     charactermap: ['./src/components/character-map/CharacterMap.tsx'],
                     outlook: ['./src/components/outlook-express/OutlookExpress.tsx'],
                     picturefax: ['./src/components/picture-viewer/PictureFaxViewer.tsx'],
+                    helpandsupport: ['./src/components/help-and-support/HelpAndSupport.tsx'],
                 }
             }
         }
